3c31390bc2616ed35c6ded8fbf1fbf8ea67803aa
ilyas <oulkaddailyas@gmail.com>
2024-11-14 17:13:52 +0100

    fix pca

--- src/pca.py
+++ src/pca.py
@@ -1,8 +1,8 @@
 from datasets import load_dataset
-from gensim.models import KeyedVectors
 import gensim.downloader as api
 import numpy as np
 from sklearn.decomposition import PCA
+
 
 def preprocess_text(text):
     tokens = text.split()
@@ -19,15 +19,20 @@ def intrinsic_dimension(dataset_path, embed, threshold=0.95):
     dataset = load_dataset(dataset_path)
     threshold = 0.95
     dimensions = []
-    for sample in dataset['train'].select(range(1000)):
+    for sample in dataset['train'].select(range(10)):
         vectors = embed_text(sample["text"], embed)
         prev = len(dimensions)
-        for i in range(2, 25):
-            pca = PCA(n_components=i)
-            pca.fit(vectors)
-            explained_variance = np.sum(pca.explained_variance_ratio_)
-            if explained_variance > threshold:
-                dimensions.append(i)
+        pca = PCA(n_components=25)
+        pca.fit(vectors)
+        explained_variance = pca.explained_variance_ratio_
+        cumul_var = 0
+        dim = 0
+        for variance in explained_variance:
+            cumul_var += variance
+            dim += 1
+
+            if cumul_var > threshold:
+                dimensions.append(dim)
                 break
 
         if prev == len(dimensions):
@@ -39,8 +44,11 @@ def intrinsic_dimension(dataset_path, embed, threshold=0.95):
 
 
 if __name__ == "__main__":
-    model = api.load("glove-twitter-25")
-    i_dim_human = intrinsic_dimension("imdb", model)
-    i_dim_ai = intrinsic_dimension("HuggingFaceTB/cosmopedia-100k", model)
+    human_dataset_path = "imdb"
+    ai_dataset_path = "HuggingFaceTB/cosmopedia-100k"
+    word2vec_path = "glove-twitter-25"
+    model = api.load(word2vec_path)
+    i_dim_human = intrinsic_dimension(human_dataset_path, model)
+    i_dim_ai = intrinsic_dimension(ai_dataset_path, model)
     print(i_dim_human, i_dim_ai)
 
