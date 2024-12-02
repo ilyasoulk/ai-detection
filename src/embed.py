b5ad2fb1ae74ed6db01b6cbc8a310cc2312cf683
ilyas <oulkaddailyas@gmail.com>
2024-12-02 22:55:45 +0100

    restructure project

---
+++ src/embed.py
@@ -0,0 +1,50 @@
+import nltk
+import torch
+import gensim.downloader as api
+from transformers import AutoModel, AutoTokenizer
+from sentence_transformers import SentenceTransformer
+
+def preprocess_text(text):
+    tokens = text.split()
+    return tokens
+
+
+def word2vec_emb(texts, embedding_model):
+    model = api.load(embedding_model)
+    res = []
+    for text in texts:
+        tokens = preprocess_text(text)
+        vectors = torch.Tensor([model[token] for token in tokens if token in model])
+        res.append(vectors)
+    return res
+
+
+def token_transformer_emb(texts, embedding_model):
+    tokenizer = AutoTokenizer.from_pretrained(embedding_model)
+    embedder = AutoModel.from_pretrained(embedding_model)
+
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    tokens = tokenizer(texts, return_tensors="pt", truncation=True, padding=True)
+
+    with torch.no_grad():
+        outputs = embedder(**tokens)
+
+    embeddings = outputs.last_hidden_state
+
+    return embeddings
+
+
+def sentence_transformer_emb(texts, embedding_model):
+    split_texts = [nltk.sent_tokenize(text) for text in texts]
+
+    embedder = SentenceTransformer(embedding_model)
+    
+    grouped_embeddings = [
+        embedder.encode(sentences)
+        for sentences in split_texts
+    ]
+
+    return grouped_embeddings
+
--- src/pca.py
+++ src/pca.py
@@ -1,56 +1,12 @@
 from datasets import load_dataset
-import gensim.downloader as api
 import numpy as np
 from sklearn.decomposition import PCA
-from sentence_transformers import SentenceTransformer
-import torch
-from transformers import AutoModel, AutoTokenizer
 import nltk
-
-
-def preprocess_text(text):
-    tokens = text.split()
-    return tokens
-
-
-def word2vec_emb(texts, embedding_model):
-    model = api.load(embedding_model)
-    res = []
-    for text in texts:
-        tokens = preprocess_text(text)
-        vectors = torch.Tensor([model[token] for token in tokens if token in model])
-        res.append(vectors)
-    return res
-
-
-def token_transformer_emb(texts, embedding_model):
-    tokenizer = AutoTokenizer.from_pretrained(embedding_model)
-    embedder = AutoModel.from_pretrained(embedding_model)
-
-    if tokenizer.pad_token_id is None:
-        tokenizer.pad_token_id = tokenizer.eos_token_id
-
-    tokens = tokenizer(texts, return_tensors="pt", truncation=True, padding=True)
-
-    with torch.no_grad():
-        outputs = embedder(**tokens)
-
-    embeddings = outputs.last_hidden_state
-
-    return embeddings
-
-
-def sentence_transformer_emb(texts, embedding_model):
-    split_texts = [nltk.sent_tokenize(text) for text in texts]
-
-    embedder = SentenceTransformer(embedding_model)
-    
-    grouped_embeddings = [
-        embedder.encode(sentences)
-        for sentences in split_texts
-    ]
-
-    return grouped_embeddings
+from embed import (
+    word2vec_emb,
+    token_transformer_emb,
+    sentence_transformer_emb
+)
 
 
 def intrinsic_dimension(texts, embedding_model, embedding_type, threshold=0.95, elements=100):
