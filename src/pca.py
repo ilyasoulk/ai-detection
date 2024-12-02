43a5d0883b8050ee47be600ff826df0479c0c2b4
ilyas <oulkaddailyas@gmail.com>
2024-12-02 21:04:52 +0100

    use more dense dataset for human generations

--- src/pca.py
+++ src/pca.py
@@ -27,6 +27,9 @@ def token_transformer_emb(texts, embedding_model):
     tokenizer = AutoTokenizer.from_pretrained(embedding_model)
     embedder = AutoModel.from_pretrained(embedding_model)
 
+    if tokenizer.pad_token_id is None:
+        tokenizer.pad_token_id = tokenizer.eos_token_id
+
     tokens = tokenizer(texts, return_tensors="pt", truncation=True, padding=True)
 
     with torch.no_grad():
@@ -50,11 +53,9 @@ def sentence_transformer_emb(texts, embedding_model):
     return grouped_embeddings
 
 
-def intrinsic_dimension(dataset_path, embedding_model, embedding_type, threshold=0.95, elements=100):
-    ds = load_dataset(dataset_path, split="train")
-    texts = ds["text"][:elements]
-
+def intrinsic_dimension(texts, embedding_model, embedding_type, threshold=0.95, elements=100):
     # Compute embeddings based on the specified embedding type
+    texts = texts[:elements]
     if embedding_type == "word2vec":
         embeddings = word2vec_emb(texts, embedding_model)
         n_components = 25
@@ -95,26 +96,38 @@ def intrinsic_dimension(dataset_path, embedding_model, embedding_type, threshold
 
 if __name__ == "__main__":
     nltk.download("punkt")
-    human_dataset_path = "imdb"
+    human_dataset_path = "abisee/cnn_dailymail"
     ai_dataset_path = "HuggingFaceTB/cosmopedia-100k"
     word2vec_path = "glove-twitter-25"
     sentence_transformers_path = "multi-qa-mpnet-base-dot-v1"
     word_transformer_path = "bert-base-uncased"
+
+    ai_ds = load_dataset(ai_dataset_path, split="train")
+    hm_ds = load_dataset(human_dataset_path, '3.0.0', split="train")
+    ai_ds = ai_ds["text"]
+    hm_ds = hm_ds["article"]
+
     # intrinsic_dimension(human_dataset_path, sentence_transformers_path, "sentence_transformer", elements=100)
-    avg_dim_ai_bert = intrinsic_dimension(ai_dataset_path, word_transformer_path, "token_transformer")
-    avg_dim_human_bert = intrinsic_dimension(human_dataset_path, word_transformer_path, "token_transformer")
-    avg_dim_human_word2vec = intrinsic_dimension(human_dataset_path, word2vec_path, "word2vec")
-    avg_dim_ai_word2vec = intrinsic_dimension(ai_dataset_path, word2vec_path, "word2vec")
+    avg_dim_ai_bert = intrinsic_dimension(ai_ds, word_transformer_path, "token_transformer")
+    avg_dim_human_bert = intrinsic_dimension(hm_ds, word_transformer_path, "token_transformer")
+    avg_dim_human_word2vec = intrinsic_dimension(ai_ds, word2vec_path, "word2vec")
+    avg_dim_ai_word2vec = intrinsic_dimension(hm_ds, word2vec_path, "word2vec")
 
 
     print("Results")
-    print(f"Average dimension using bert on synthetic dataset : {avg_dim_ai_bert}")
-    print(f"Average dimension using bert on human dataset : {avg_dim_human_bert}")
+    print(f"Average dimension using bert on synthetic dataset {ai_dataset_path.split('/')[-1]} : {avg_dim_ai_bert}")
+    print(f"Average dimension using bert on human dataset {human_dataset_path.split('/')[-1]}: {avg_dim_human_bert}")
     print(f"Average dimension using word2vec on synthetic dataset : {avg_dim_ai_word2vec}")
     print(f"Average dimension using word2vec on human dataset : {avg_dim_human_word2vec}")
 
 # Results
-# Average dimension using bert on synthetic dataset : 208.24
-# Average dimension using bert on human dataset : 169.36
-# Average dimension using word2vec on synthetic dataset : 19.2
-# Average dimension using word2vec on human dataset : 17.55
+
+# Average dimension using bert on synthetic dataset cosmopedia-100k : 208.24
+# Average dimension using bert on human dataset imdb : 169.36
+# Average dimension using word2vec on synthetic dataset cosmopedia-100k : 19.2
+# Average dimension using word2vec on human dataset imdb : 17.55
+
+# Average dimension using bert on synthetic dataset cosmopedia-100k : 208.24
+# Average dimension using bert on human dataset cnn_dailymail: 213.11
+# Average dimension using word2vec on synthetic dataset cosmopedia-100k : 18.69
+# Average dimension using word2vec on human dataset cnn_dailymail : 19.2
