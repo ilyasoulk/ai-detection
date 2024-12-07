4f422b9f73af831060e15ab20f47022aa5b71685
Camil Ziane <camil.ziane@epita.fr>
2024-12-07 20:37:14 +0100

    feat: generate dataset

---
+++ Makefile
@@ -0,0 +1,13 @@
+generate_SmolLM2-1.7B_dataset:
+	python3 /home/yanel/epita/ai-detection/src/generate.py --config ./configs/SmolLM2-1.7B-Instruct.yaml
+
+generate_Phi-3.5_dataset:
+	python3 /home/yanel/epita/ai-detection/src/generate.py --config ./configs/Phi-3.5-mini-instruct.yaml
+
+generate_Qwen2.5-1.5B_dataset:
+	python3 /home/yanel/epita/ai-detection/src/generate.py --config ./configs/Qwen2.5-1.5B-Instruct.yamldataset_path
+
+generate_SmolLM-360M_dataset:
+	python3 /home/yanel/epita/ai-detection/src/generate.py --config ./configs/SmolLM-360M-Instruct.yaml
+
+
--- src/generate.py
+++ src/generate.py
@@ -1,112 +1,117 @@
 from datasets import load_dataset
-from dotenv import load_dotenv
-from openai import OpenAI
-from tqdm import tqdm
+import argparse
+from torch.utils.data import DataLoader
 import random
-import time
-import json
 import os
-from concurrent.futures import ThreadPoolExecutor, as_completed
+import json
+import yaml
 
 
-def generate_prompt(truncated_text):
-    messages = [
-        {"role": "system", "content": (
-            "You are an article completion assistant. You MUST ALWAYS format your response with [ARTICLE] and [/ARTICLE] tokens. "
-            "Your completion should be natural and coherent with the given text."
-            "Your completion should be detailled enough to be a daily news article"
-        )},
-        {"role": "user", "content": (
-            "Complete the following article. Your response MUST:\n"
-            "1. Start with [ARTICLE]\n"
-            "2. End with [/ARTICLE]\n"
-            "3. Include the given text at the start\n"
-            "4. Detail the article as much as needed\n"
-            "5. Complete it naturally\n\n"
-            "Here's the text to complete:\n"
-            "[ARTICLE]\n"
-            f"{truncated_text}\n"
-            "[/ARTICLE]"
-        )},
-        {"role": "assistant", "content": (
-            "I understand that I must:\n"
-            "- Keep the [ARTICLE] and [/ARTICLE] tokens\n"
-            "- Include the original text\n"
-            "- Detail the article as much as needed\n"
-            "- Complete it naturally\n"
-            "I will now provide the completion."
-        )},
-        {"role": "user", "content": "Please provide the completion now:"}
-    ]
+def generate_prompts(article: str, nb_chars: int):
+    with open(config["prompt_path"], "r") as f:
+        messages = json.load(f)
+    for message in messages:
+        if "content" in message and isinstance(message["content"], str):
+            message["content"] = message["content"].format(
+                article=article, nb_chars=nb_chars
+            )
     return messages
 
-def parse_article_content(text):
-    start_token = "[ARTICLE]"
-    end_token = "[/ARTICLE]"
-    
-    start_index = text.find(start_token) + len(start_token)
-    end_index = text.find(end_token)
-    
-    if start_index == -1 or end_index == -1:
-        return ""
-        
-    return text[start_index:end_index].strip()
 
-
-def send_openai(message, model="gpt-3.5-turbo-0125", max_tokens=800, temperature=1):
-    response = client.chat.completions.create(
-        model=model,
-        messages=message,
-        max_tokens=max_tokens,
-        temperature=temperature,
-        top_p=0.9
-    )
-    return response.choices[0].message.content
-
-
-def random_truncate(text, min_n=30, max_n=50):
+def random_split(text) -> tuple[str, str, int]:
+    min_n = len(text) // 4
+    max_n = len(text) // 2
     n = random.randint(min_n, max_n)
     if n >= len(text):
         return text
-    return text[:n]
+    prefix = text[:n]
+    suffix = text[n:]
+    return prefix, suffix
 
-def process_article(article):
-    prefix = random_truncate(article)
-    prompt = generate_prompt(prefix)
-    response = send_openai(prompt)
-    return {
-        "ai": parse_article_content(response),
-        "human": article
-    }
 
-def generate(dataset_path="abisee/cnn_dailymail", filepath="data/synthetic_reporter.json"):
-    ds = load_dataset(dataset_path, '3.0.0', split='train')
-    articles = ds["article"]
+def process_articles(articles):
+    prefixes, suffixes = zip(*[random_split(article) for article in articles])
+    messages = [
+        generate_prompts(prefix, len(suffix))
+        for prefix, suffix in zip(prefixes, suffixes)
+    ]
+    answers = generate_batch_answer(messages)
+    return [
+        {"ai": answer, "human": suffix} for answer, suffix in zip(answers, suffixes)
+    ]
 
-    # create file if does not exist
-    os.makedirs(os.path.dirname(filepath), exist_ok=True)
 
-    data = []
-    with ThreadPoolExecutor(max_workers=5) as executor:
-        futures = {executor.submit(process_article, article): article for article in articles}
-        for future in tqdm(as_completed(futures), total=len(articles), desc="Generating the dataset..."):
-            try:
-                sample = future.result()
-                data.append(sample)
-            except Exception as e:
-                print(f"Error processing article: {e}")
+def get_dataloader(dataset_path: str, batch_size: int = 32):
+    ds = load_dataset(path=dataset_path, name="3.0.0", split="train")
+    ds.set_format(type="torch", columns=["article"])
+    dataloader = DataLoader(ds, batch_size=batch_size, shuffle=True)
+    return dataloader
 
-            # Save periodically
-            if len(data) % 10 == 0:
-                with open(filepath, 'w') as f:
-                    json.dump(data, f, indent=4)
 
-    # Final save
-    with open(filepath, 'w') as f:
-        json.dump(data, f, indent=4)
+def generate_dataset():
+    dataloader = get_dataloader(
+        dataset_path=config["dataset_path"], batch_size=config["batch_size"]
+    )
+    datas = []
+    for batch in dataloader:
+        articles = batch["article"]
+        ai_human_articles = process_articles(articles)
+        datas.extend(ai_human_articles)
+    directory = f'{config["directory"]}/{config["model"]}'
+    os.makedirs(directory, exist_ok=True)
+    with open(f"{directory}/synthetic_reporter.json", "w") as f:
+        json.dump(datas, f)
+    with open(f"{directory}/config.yaml", "w") as f:
+        yaml.dump(config, f)
+
+
+def load_config(config_path):
+    with open(config_path, "r") as f:
+        return yaml.safe_load(f)
+
+
+def parse_and_load_config():
+    parser = argparse.ArgumentParser(description="Load config for dataset processing")
+    parser.add_argument(
+        "--config", type=str, required=True, help="Path to the config file"
+    )
+    parser.add_argument(
+        "--dataset_path",
+        type=str,
+        default="abisee/cnn_dailymail",
+        help="Path to the dataset",
+    )
+    parser.add_argument(
+        "--directory", type=str, default="data", help="Directory to save outputs"
+    )
+    parser.add_argument(
+        "--prompt_path",
+        type=str,
+        default="prompts/complete_articles.json",
+        help="Path to the prompt file",
+    )
+    args = parser.parse_args()
+
+    config = load_config(args.config)
+    config["dataset_path"] = args.dataset_path
+    config["directory"] = args.directory
+    config["prompt_path"] = args.prompt_path
+
+    return config
+
 
 if __name__ == "__main__":
-    load_dotenv()
-    openai_key = os.getenv("OPENAI_KEY")
-    client = OpenAI(api_key=openai_key)
-    generate()
+    config = parse_and_load_config()
+    from vllm import LLM, SamplingParams
+    from vllm.entrypoints.chat_utils import ChatCompletionMessageParam
+
+    llm = LLM(model=config["model"])
+    sampling_params = SamplingParams(**config["sampling_params"])
+
+    def generate_batch_answer(
+        messages: list[list[ChatCompletionMessageParam]],
+    ):
+        res = llm.chat(messages=messages, sampling_params=sampling_params)
+        return [res.outputs[-1].text for res in res]
+
+    generate_dataset()
